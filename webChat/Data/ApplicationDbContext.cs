1b7e8bae0123757d8569aa298206f5a6e6bb8aed
gongueiro1 <ggoncalosilva1@gmail.com>
2026-05-21 16:10:40 +0100

    começar a pensar na conta de Admin

--- webChat/Data/ApplicationDbContext.cs
+++ webChat/Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     public DbSet<Post> Posts { get; set; }
     public DbSet<Comment> Comments { get; set; }
     public DbSet<PostLike> PostLikes { get; set; }
+    public DbSet<Report> Reports { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
